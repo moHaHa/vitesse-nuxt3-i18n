ba59bf0e3a30d655638b306de1260fc4a0dfd529
moHaHa <diab.4869@gmail.com>
2023-05-08 09:29:21 +0300

    feat : about page

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div text="xl gray4" m-5 flex="~ gap3" justify-center>
     <NuxtLink i-carbon-campsite to="/" />
+    <NuxtLink i-carbon-book to="/about" />
     <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
     <DarkToggle />
   </div>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const online = useOnline()
+</script>
+
+<template>
+  <div>
+    <div>
+      it's just a title
+    </div>
+  </div>
+</template>
