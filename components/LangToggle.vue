485ba5abe69d3bc7d5e530aa737e88a44d22725a
moHaHa <diab.4869@gmail.com>
2023-05-12 10:15:05 +0300

    feat : toggle lang

---
+++ components/LangToggle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { locale } = useI18n()
+
+function toggleLang() {
+  locale.value = locale.value === 'ar' ? 'en' : 'ar'
+}
+</script>
+
+<template>
+  <button class="!outline-none" @click="toggleLang">
+    <div class="i-carbon-language" /> {{ locale }}
+  </button>
+</template>
